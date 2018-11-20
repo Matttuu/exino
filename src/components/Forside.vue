f51e0244a3c0488b174e8d9a96af915ee37fac89
Mattu <xuniqh@gmail.com>
2018-11-20 22:21:32 +0100

    Fixed background

--- src/components/Forside.vue
+++ src/components/Forside.vue
@@ -1,7 +1,6 @@
 <template>
-
-  
-
+<div id ="forside_bg">
+  </div>
 </template>
 
 <script>
@@ -16,6 +15,19 @@ export default {
 </script>
 
 <style scoped>
-
+#forside_bg{
+  min-height:100%;
+  min-width:1024px;
+  width:100%;
+  height:auto;
+  position:fixed;
+  top: 0;
+  left: 0;
+  background: url('https://www.placehold.it/1920x1080.png') no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
 
 </style>
